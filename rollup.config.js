9c4c9a1d5e04e3a65594b8d336db29b402bce634
Pedr Browne <pedr.browne@gmail.com>
2017-09-18 18:59:57 +0100

    Fix formatting in rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,17 @@
-import resolve from "rollup-plugin-node-resolve";
-import babel from "rollup-plugin-babel";
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
 
 // Trasnpiled using Babel
 export default {
-  input: "src/index.js",
+  input: 'src/index.js',
   output: {
-    file: "lib/index.js",
-    format: "cjs" // Use Common JS Modules
+    file: 'lib/index.js',
+    format: 'cjs', // Use Common JS Modules
   },
   plugins: [
     resolve(),
     babel({
-      exclude: "node_modules/**" // only transpile project source
-    })
-  ]
+      exclude: 'node_modules/**', // only transpile project source
+    }),
+  ],
 };
