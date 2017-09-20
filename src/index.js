7a7b26c0d83765ab90904eb0cadd76b8d29d607b
Pedr Browne <pedr.browne@gmail.com>
2017-09-20 19:03:56 +0100

    Add all factories to index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import grid from './grid';
+import cell from './cell';
+import point from './point';
+import dimensions from './dimensions';
+import iterator from './iterator';
 
-export { grid };
+export { grid, cell, point, dimensions, iterator };
