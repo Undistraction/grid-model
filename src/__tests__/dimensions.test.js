1ac741ae1e07ed1eb5ddae4a61983c72ac042e48
Pedr Browne <pedr.browne@gmail.com>
2017-09-21 09:06:13 +0100

    Remove percentage string handling from dimensions
    Add back later if needed

--- src/__tests__/dimensions.test.js
+++ src/__tests__/dimensions.test.js
@@ -1,19 +1,19 @@
 import createDimensions, {
   INVALID_PARAMS_MESSAGE,
-  INVALID_DIMENSIONS_MESSAGE,
+  INCORRECT_NUMBER_OF_PARAMS_MESSAGE,
 } from '../dimensions';
 
 describe('dimensions', () => {
   describe('invalid params', () => {
-    it('throws and error if only width, only height, or only aspectRatio are supplied', () => {
+    it('throws an error if only width, only height, or only aspectRatio are supplied', () => {
       expect(() => createDimensions({ width: 100 })).toThrowError(
-        new RegExp(INVALID_DIMENSIONS_MESSAGE)
+        new RegExp(INCORRECT_NUMBER_OF_PARAMS_MESSAGE)
       );
       expect(() => createDimensions({ height: 100 })).toThrowError(
-        new RegExp(INVALID_DIMENSIONS_MESSAGE)
+        new RegExp(INCORRECT_NUMBER_OF_PARAMS_MESSAGE)
       );
       expect(() => createDimensions({ aspectRatio: 0.5 })).toThrowError(
-        new RegExp(INVALID_DIMENSIONS_MESSAGE)
+        new RegExp(INCORRECT_NUMBER_OF_PARAMS_MESSAGE)
       );
     });
   });
@@ -26,14 +26,14 @@ describe('dimensions', () => {
 
   it("throws error if width or height aren't valid numbers or percentages", () => {
     expect(() =>
-      createDimensions({ width: '111%', height: '44%' })
+      createDimensions({ width: 111, height: 44 })
     ).not.toThrowError();
 
     expect(() => createDimensions({ width: 'aaa', height: 10 })).toThrowError(
       new RegExp(INVALID_PARAMS_MESSAGE)
     );
 
-    expect(() => createDimensions({ width: null, height: 10 })).toThrowError(
+    expect(() => createDimensions({ width: 33, height: '10' })).toThrowError(
       new RegExp(INVALID_PARAMS_MESSAGE)
     );
   });
@@ -44,7 +44,7 @@ describe('dimensions', () => {
     ).toThrowError(new RegExp(INVALID_PARAMS_MESSAGE));
   });
 
-  it.only('stores width and height', () => {
+  it('stores width and height', () => {
     const instance = createDimensions({ width: 100, height: 200 });
     expect(instance.width).toBe(100);
     expect(instance.height).toBe(200);
@@ -56,16 +56,23 @@ describe('dimensions', () => {
     expect(instance.height).toBe(0);
   });
 
-  it('ignores supplied aspectRatio if width and height are supplied', () => {
-    const instance = createDimensions({
-      width: 100,
-      height: 200,
-      aspectRatio: 2,
-    });
-    expect(instance.width).toBe(100);
-    expect(instance.height).toBe(200);
-    expect(instance.aspectRatio).toBe(0.5);
-  });
+  // it.only(
+  //   'ignores supplied aspectRatio if width and height are supplied',
+  //   () => {
+  //     const instance = createDimensions({
+  //       width: 100,
+  //       height: 200,
+  //       aspectRatio: 66,
+  //     });
+  //     expect(() =>
+  //       createDimensions({
+  //         width: 100,
+  //         height: 200,
+  //         aspectRatio: 66,
+  //       })
+  //     ).toThrowError(INCORRECT_NUMBER_OF_PARAMS_MESSAGE);
+  //   }
+  // );
 
   it('calculates width if only height and aspectRatio are supplied', () => {
     const instance = createDimensions({ height: 200, aspectRatio: 0.5 });
--- src/dimensions.js
+++ src/dimensions.js
@@ -1,34 +1,33 @@
 import { isNumber } from 'lodash';
+import { isPositiveNumber } from './validations';
 
-import { isNumberOrPercentString, isPositiveNumber } from './validations';
-
-export const MISSING_PARAMS_MESSAGE =
-  'You must supply at least two arguments of: [width, height, aspectRatio]';
+export const INCORRECT_NUMBER_OF_PARAMS_MESSAGE =
+  'You must supply exactly two of: width, height, aspectRatio';
 export const INVALID_PARAMS_MESSAGE = 'Parameter was invalid:';
 
 const throwInvalidParamError = (param, value) => {
   throw new Error(`${INVALID_PARAMS_MESSAGE} ${param}: ${value}`);
 };
 
-const throwMissingParamsError = () => {
-  throw new Error(MISSING_PARAMS_MESSAGE);
+const throwIncorrectNumberOfParamsError = () => {
+  throw new Error(INCORRECT_NUMBER_OF_PARAMS_MESSAGE);
 };
 
 const createDimensions = ({ width, height, aspectRatio }) => {
-  // Validate we have the params we need
-  if ([width, height, aspectRatio].filter(x => isNumber(x)).length < 2) {
-    throwMissingParamsError();
+  // Validate supplied params are valid
+  if (width && !isNumber(width)) {
+    throwInvalidParamError('width', width);
   }
 
-  // Validate supplied params are valid
-  if (width && !isNumberOrPercentString(width))
-    throwInvalidParamError('width', width);
-
-  if (height && !isNumberOrPercentString(height))
-    throwInvalidParamError('height', height);
+  if (height && !isNumber(height)) throwInvalidParamError('height', height);
 
   if (aspectRatio && !isPositiveNumber(aspectRatio))
     throwInvalidParamError('aspectRatio', aspectRatio);
+
+  // Validate we have the params we need
+  if ([width, height, aspectRatio].filter(x => isNumber(x)).length !== 2) {
+    throwIncorrectNumberOfParamsError();
+  }
 
   // ---------------------------------------------------------------------------
   // API
