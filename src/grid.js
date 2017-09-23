385d6f24878717aa6b6ca2957fd7e0a2bc31de28
Pedr Browne <pedr.browne@gmail.com>
2017-09-23 11:05:30 +0100

    Refactor into pure functions

--- src/grid.js
+++ src/grid.js
@@ -8,24 +8,15 @@ import createPoint from './point';
 // Error Messages
 // -----------------------------------------------------------------------------
 
-export const CANT_DERIVE_CELL_DIMENSIONS_MESSAGE =
-  "Grid Couldn't calculate cell dimensions from supplied arguments";
-
-export const CANT_DERIVE_GRID_DIMENSIONS_MESSAGE =
-  "Grid Couldn't calculate rows and columns from supplied arguments";
-
 export const INVALID_PARAMS_MESSAGE =
-  'You must supply columns OR cellWidth and rows OR cellHeight';
+  "You didn't supply sufficient params to derive a valid grid";
 
-export const INVALID_GRID_DIMENSIONS_MESSAGE =
-  'Using the values supplied to calculate rows and columns would result in an invalid grid';
-
-export const INVALID_GUTTER_DIMENSIONS_MESSAGE =
-  'The supplied gutter dimensions are incompatible with the other params';
+export const CONFLICTING_PARAMS_MESSAGE =
+  'You supplied params that cannot be reconciled to a valid grid';
 
 export const INVALID_CELL_INDEX_MESSAGE = 'The cell index supplied was invalid';
 
-export const INVALID_PARAMS_TO_CELL_AT_MESSAGE =
+export const INVALID_CELL_LOCATION_MESSAGE =
   'You must supply an x and y position to cellAt()';
 
 export const INVALID_COLUMN_INDEX = 'The column index was invalid';
@@ -69,6 +60,213 @@ const regionForCells = cells => {
 const validParamCount = params =>
   params.filter(param => !isNil(param) && param !== '').length;
 
+/**
+ * 
+ * @param {number} width The width of the grid.
+ * @param {number} height The height of the grid.
+ * @param {number} aspectRatio the aspect ratio of the grid.
+ * @param {number} rows the number of rows in the grid.
+ * @param {number} columns the number of columns in the grid.
+ * @param {number} cellWidth the width of cell in the grid.
+ * @param {number} cellHeight the height of a cell in the grid.
+ * 
+ * Validates the params to ensure enough information was supplied to derive a
+ * valid grid.
+ * 
+ * @returns {null} Nothing is returned.
+ */
+const validateParams = (
+  width,
+  height,
+  aspectRatio,
+  rows,
+  columns,
+  cellWidth,
+  cellHeight
+) => {
+  // // Without columns we must have a cellWidth to derive the
+  // if (
+  //   (!isNumber(columns) && !isNumber(cellWidth)) ||
+  //   (!isNumber(rows) && !isNumber(cellHeight))
+  // ) {
+  //   throw new Error(INVALID_PARAMS_MESSAGE);
+  // }
+
+  // Guard against cellWidth or cellHeight that would break grid;
+  if (
+    (isNumber(columns) && isNumber(cellWidth) && isNumber(width)) ||
+    (isNumber(rows) && isNumber(cellHeight) && isNumber(height))
+  ) {
+    if (columns * cellWidth > width || rows * cellHeight > height) {
+      throw new Error(CONFLICTING_PARAMS_MESSAGE);
+    }
+  }
+};
+
+const canDeriveWidth = (cellHeight, rows) =>
+  !!(isNumber(cellHeight) && isNumber(rows));
+const canDeriveHeight = (cellWidth, columns) =>
+  !!(isNumber(cellWidth) && isNumber(columns));
+const deriveHeight = (rows, cellHeight, gutterHeight) =>
+  cellHeight * rows + gutterHeight * (rows - 1);
+const deriveWidth = (columns, cellWidth, gutterWidth) =>
+  cellWidth * columns + gutterWidth * (columns - 1);
+
+const deriveCellWidth = (width, gutterWidth, columns) =>
+  (width - gutterWidth * (columns - 1)) / columns;
+const deriveCellHeight = (height, gutterHeight, rows) =>
+  (height - gutterHeight * (rows - 1)) / rows;
+
+const canDeriveColumns = (width, cellWidth) =>
+  !!(isNumber(width) && isNumber(cellWidth));
+const canDeriveRows = (height, cellHeight) =>
+  !!(isNumber(height) && isNumber(cellHeight));
+const deriveColumns = (width, cellWidth) => width / cellWidth;
+const deriveRows = (height, cellHeight) => height / cellHeight;
+
+const isValidColumnIndex = (columnIndex, totalColumns) =>
+  columnIndex <= totalColumns - 1;
+const isValidRowIndex = (rowIndex, totalRows) => rowIndex <= totalRows - 1;
+
+const topLeftPointForCell = (
+  x,
+  y,
+  cellWidth,
+  cellHeight,
+  gutterWidth,
+  gutterHeight
+) => {
+  const xPos = (cellWidth + gutterWidth) * x;
+  const yPos = (cellHeight + gutterHeight) * y;
+  return createPoint(xPos, yPos);
+};
+
+// /**
+//  *
+//  * @param {number} width The
+//  * @param {number} height
+//  * @param {number} aspectRatio
+//  * @param {number} rows
+//  * @param {number} columns
+//  * @param {number} cellWidth
+//  * @param {number} cellHeight
+//  * @param {number} gutterWidth
+//  * @param {number} gutterHeight
+//  *
+// * Create a dimensions object to represent the width and height of the grid.
+//    * If this information hasn't been explicly supplied, try to derive that info
+//    * from the other parameters supplied. If there is no way to assertain the
+//    * dimensions, throw an Error.
+//    *
+//    * @returns {object} dimensions
+//  */
+const calculateDimensions = (
+  width,
+  height,
+  aspectRatio,
+  rows,
+  columns,
+  cellWidth,
+  cellHeight,
+  gutterWidth,
+  gutterHeight
+) => {
+  if (!isNumber(width) && !isNumber(aspectRatio)) {
+    if (canDeriveWidth(cellHeight, rows)) {
+      width = deriveWidth(columns, cellWidth, gutterWidth);
+    }
+  }
+
+  if (!isNumber(height) && !isNumber(aspectRatio)) {
+    if (canDeriveHeight(cellWidth, columns)) {
+      height = deriveHeight(rows, cellHeight, gutterHeight);
+    }
+  }
+
+  // If we don't have at least two params, throw an Error.
+  if (validParamCount([width, height, aspectRatio]) < 2) {
+    throw new Error(INVALID_PARAMS_MESSAGE);
+  }
+
+  return createDimensions({ width, height, aspectRatio });
+};
+
+const calcualteGridDimensions = (
+  columns,
+  rows,
+  dimensions,
+  cellWidth,
+  cellHeight
+) => {
+  const c = isNumber(columns)
+    ? columns
+    : canDeriveColumns(dimensions.width, cellWidth) &&
+      deriveColumns(dimensions.width, cellWidth);
+  const r = isNumber(rows)
+    ? rows
+    : canDeriveRows(dimensions.height, cellHeight) &&
+      deriveRows(dimensions.height, cellHeight);
+  if (!r || !c) {
+    throw new Error(INVALID_PARAMS_MESSAGE);
+  }
+
+  const rowParts = r.toString().split('.');
+  const columnParts = c.toString().split('.');
+
+  const wholeRows = Number(rowParts[0]);
+  const wholeColumns = Number(columnParts[0]);
+
+  return createDimensions({
+    width: wholeColumns,
+    height: wholeRows,
+  });
+};
+
+const calculateCellDimensions = (
+  cellWidth,
+  cellHeight,
+  gutterWidth,
+  gutterHeight,
+  dimensions,
+  gridDimensions
+) => {
+  const width = isNumber(cellWidth)
+    ? cellWidth
+    : deriveCellWidth(dimensions.width, gutterWidth, gridDimensions.width);
+  const height = isNumber(cellHeight)
+    ? cellHeight
+    : deriveCellHeight(dimensions.height, gutterHeight, gridDimensions.height);
+
+  return createDimensions({ width, height });
+};
+
+const calculateGutterDimensions = (
+  gutterWidth,
+  gutterHeight,
+  dimensions,
+  gridDimensions,
+  cellDimensions
+) => {
+  let width = gutterWidth;
+  let height = gutterHeight;
+
+  const remainingHSpace =
+    dimensions.width - gridDimensions.width * cellDimensions.width;
+  const remainingVSpace =
+    dimensions.height - gridDimensions.height * cellDimensions.height;
+
+  const hGutter = remainingHSpace / (gridDimensions.width - 1);
+  const vGutter = remainingVSpace / (gridDimensions.height - 1);
+
+  width = hGutter;
+  height = vGutter;
+
+  return createDimensions({
+    width,
+    height,
+  });
+};
+
 // -----------------------------------------------------------------------------
 // Entry
 // -----------------------------------------------------------------------------
@@ -87,193 +285,88 @@ const createGrid = (
     gutter,
   } = {}
 ) => {
-  let dimensions;
-  let gridDimensions;
-  let cellDimensions;
-  let gutterDimensions;
-
-  const getDimensions = () => {
-    dimensions = dimensions || saveDimensions();
-    return dimensions;
-  };
-
-  const getGridDimensions = () => {
-    gridDimensions = gridDimensions || saveGridDimensions();
-    return gridDimensions;
-  };
-
-  const getCellDimensions = () => {
-    cellDimensions = cellDimensions || saveCellDimensions();
-    return cellDimensions;
-  };
-
-  const getGutterDimensions = () => {
-    gutterDimensions = gutterDimensions || saveGutterDimensions();
-    return gutterDimensions;
-  };
-
-  const canDeriveWidth = () => !!(cellHeight && rows);
-  const canDeriveHeight = () => !!(cellWidth && columns);
-  const deriveHeight = () => cellHeight * rows + gutterHeight * (rows - 1);
-  const deriveWidth = () => cellWidth * columns + gutterWidth * (columns - 1);
-
-  const canDeriveCellWidth = () => !!(getDimensions().width && columns);
-  const canDeriveCellHeight = () => !!(getDimensions().height && rows);
-  const deriveCellWidth = () =>
-    (getDimensions().width - gutterWidth * (columns - 1)) / columns;
-  const deriveCellHeight = () =>
-    (getDimensions().height - gutterHeight * (rows - 1)) / rows;
-
-  const canDeriveColumns = () => width && cellWidth;
-  const canDeriveRows = () => height && cellHeight;
-  const deriveColumns = () => getDimensions().width / cellWidth;
-  const deriveRows = () => getDimensions().height / cellHeight;
-
-  const validateGutters = (hGutter, vGutter) =>
-    gutterWidth === 0 ||
-    gutterWidth === hGutter ||
-    (gutterHeight === 0 || gutterHeight === vGutter);
-
-  const isValidColumnIndex = columnIndex =>
-    columnIndex <= getGridDimensions().width - 1;
-  const isValidRowIndex = rowIndex =>
-    rowIndex <= getGridDimensions().height - 1;
-
-  const topLeftPointForCell = (x, y) => {
-    const xPos = (getCellDimensions().width + getGutterDimensions().width) * x;
-    const yPos =
-      (getCellDimensions().height + getGutterDimensions().height) * y;
-    return createPoint(xPos, yPos);
-  };
-
-  /**
-   * Create a dimensions object to represent the width and height of the grid.
-   * If this information hasn't been explicly supplied, try to derive that info
-   * from the other parameters supplied. If there is no way to assertain the
-   * dimensions, throw an Error.
-   * 
-   * @returns {object} dimensions
-   */
-  const saveDimensions = () => {
-    // If we only have width, we will need rowCount + cellHeight
-    if (!width && !aspectRatio) {
-      if (canDeriveWidth()) {
-        width = deriveWidth();
-      }
-    }
-
-    if (!height && !aspectRatio) {
-      if (canDeriveHeight()) {
-        height = deriveHeight();
-      }
-    }
-
-    // If we don't have at least two params, throw an Error.
-    if (validParamCount([width, height, aspectRatio]) < 2) {
-      throw new Error(INVALID_PARAMS_MESSAGE);
-    }
-
-    return createDimensions({ width, height, aspectRatio });
-  };
-
-  const saveGridDimensions = () => {
-    const c = columns || (canDeriveColumns() && deriveColumns());
-    const r = rows || (canDeriveRows() && deriveRows());
-    if (!r || !c) {
-      throw new Error(CANT_DERIVE_GRID_DIMENSIONS_MESSAGE);
-    }
-
-    const rowParts = r.toString().split('.');
-    const columnParts = c.toString().split('.');
-
-    const wholeRows = Number(rowParts[0]);
-    const wholeColumns = Number(columnParts[0]);
-
-    if (!wholeRows > 0 || !wholeColumns > 0) {
-      throw new Error(INVALID_GRID_DIMENSIONS_MESSAGE);
-    }
-
-    return createDimensions({ width: wholeColumns, height: wholeRows });
-  };
-
-  const saveCellDimensions = () => {
-    const cW = cellWidth || (canDeriveCellWidth() && deriveCellWidth());
-    const cH = cellHeight || (canDeriveCellHeight() && deriveCellHeight());
-
-    if (!isNumber(cW) || !isNumber(cH)) {
-      throw new Error(CANT_DERIVE_CELL_DIMENSIONS_MESSAGE);
-    }
-
-    return createDimensions({ width: cW, height: cH });
-  };
-
-  const saveGutterDimensions = () => {
-    let gw = gutterWidth;
-    let gh = gutterHeight;
-
-    const remainingHSpace =
-      getDimensions().width -
-      getGridDimensions().width * getCellDimensions().width;
-    const remainingVSpace =
-      getDimensions().height -
-      getGridDimensions().height * getCellDimensions().height;
-
-    const hGutter = remainingHSpace / (getGridDimensions().width - 1);
-    const vGutter = remainingVSpace / (getGridDimensions().height - 1);
-    if (!validateGutters(hGutter, vGutter)) {
-      throw new Error(INVALID_GUTTER_DIMENSIONS_MESSAGE);
-    } else {
-      gw = hGutter;
-      gh = vGutter;
-    }
-
-    const d = createDimensions({
-      width: gw,
-      height: gh,
-    });
-    return d;
-  };
+  validateParams(
+    width,
+    height,
+    aspectRatio,
+    rows,
+    columns,
+    cellWidth,
+    cellHeight,
+    gutterWidth,
+    gutterHeight,
+    gutter
+  );
 
   // Transfer gutter value to gutterWidth and gutterHeight if they aren't set
   if (!isNumber(gutterWidth)) gutterWidth = gutter || 0;
   if (!isNumber(gutterHeight)) gutterHeight = gutter || 0;
 
-  // Guard against missing params that would cause recursion
-  if ((!columns && !cellWidth) || (!rows && !cellHeight)) {
-    throw new Error(INVALID_PARAMS_MESSAGE);
-  }
+  const dimensions = calculateDimensions(
+    width,
+    height,
+    aspectRatio,
+    rows,
+    columns,
+    cellWidth,
+    cellHeight,
+    gutterWidth,
+    gutterHeight
+  );
 
-  // Guard against cellWidth or cellHeight that would break grid;
-  if ((columns && cellWidth) || (rows && cellHeight)) {
-    if (
-      columns * cellWidth > getDimensions().width ||
-      rows * cellHeight > getDimensions().height
-    ) {
-      throw new Error(INVALID_PARAMS_MESSAGE);
-    }
-  }
+  const gridDimensions = calcualteGridDimensions(
+    columns,
+    rows,
+    dimensions,
+    cellWidth,
+    cellHeight
+  );
 
-  getDimensions();
-  getGridDimensions();
-  getCellDimensions();
-  getGutterDimensions();
+  const cellDimensions = calculateCellDimensions(
+    cellWidth,
+    cellHeight,
+    gutterWidth,
+    gutterHeight,
+    dimensions,
+    gridDimensions
+  );
+  const gutterDimensions = calculateGutterDimensions(
+    gutterWidth,
+    gutterHeight,
+    dimensions,
+    gridDimensions,
+    cellDimensions
+  );
 
   // ---------------------------------------------------------------------------
   // API
   // ---------------------------------------------------------------------------
 
-  const cellCount = () => getGridDimensions().area();
+  const cellCount = () => gridDimensions.area();
 
   const regionForCellAt = (x, y) => {
     if (!isNumber(x) || !isNumber(y)) {
-      throw new Error(INVALID_PARAMS_TO_CELL_AT_MESSAGE);
+      throw new Error(INVALID_CELL_LOCATION_MESSAGE);
     }
 
-    if (x < 0 || x >= columns || y < 0 || y >= rows) {
+    if (
+      !isValidColumnIndex(x, gridDimensions.width) ||
+      !isValidRowIndex(y, gridDimensions.height)
+    ) {
       throw new Error(INVALID_CELL_INDEX_MESSAGE);
     }
 
-    return createRegion(topLeftPointForCell(x, y), getCellDimensions());
+    return createRegion(
+      topLeftPointForCell(
+        x,
+        y,
+        cellDimensions.width,
+        cellDimensions.height,
+        gutterDimensions.width,
+        gutterDimensions.height
+      ),
+      cellDimensions
+    );
   };
 
   const regionForColumns = (start, end) => {
@@ -281,20 +374,24 @@ const createGrid = (
     if (
       isNil(start) ||
       !isPositiveInteger(start) ||
-      !isValidColumnIndex(start)
+      !isValidColumnIndex(start, gridDimensions.width)
     ) {
       throw new Error(INVALID_COLUMN_INDEX);
     }
 
     // Validate end
-    if (!isNil(end) && (!isPositiveInteger(end) || !isValidColumnIndex(end))) {
+    if (
+      !isNil(end) &&
+      (!isPositiveInteger(end) ||
+        !isValidColumnIndex(end, gridDimensions.width))
+    ) {
       throw new Error(INVALID_COLUMN_INDEX);
     }
 
     const startCell = regionForCellAt(start, 0);
     const endCell = regionForCellAt(
       isNumber(end) ? end : start,
-      getGridDimensions().height - 1
+      gridDimensions.height - 1
     );
 
     return regionForCells([startCell, endCell]);
@@ -302,18 +399,25 @@ const createGrid = (
 
   const regionForRows = (start, end) => {
     // Validate start
-    if (isNil(start) || !isPositiveInteger(start) || !isValidRowIndex(start)) {
+    if (
+      isNil(start) ||
+      !isPositiveInteger(start) ||
+      !isValidRowIndex(start, gridDimensions.height)
+    ) {
       throw new Error(INVALID_COLUMN_INDEX);
     }
 
     // Validate end
-    if (!isNil(end) && (!isPositiveInteger(end) || !isValidRowIndex(end))) {
+    if (
+      !isNil(end) &&
+      (!isPositiveInteger(end) || !isValidRowIndex(end, gridDimensions.height))
+    ) {
       throw new Error(INVALID_COLUMN_INDEX);
     }
 
     const startCell = regionForCellAt(0, start);
     const endCell = regionForCellAt(
-      getGridDimensions().width - 1,
+      gridDimensions.width - 1,
       isNumber(end) ? end : start
     );
 
