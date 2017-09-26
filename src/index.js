edaaa064c2ac18209319748678e9b41569883352
Pedr Browne <pedr.browne@gmail.com>
2017-09-26 15:07:19 +0100

    New build

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import grid from './grid';
+import region from './region';
+import point from './point';
+import dimensions from './dimensions';
+import iterator from './iterator';
 
-export default grid;
+export { grid, region, point, dimensions, iterator };
